5759d6c368f8381bde6f2a543e7b347679a5c21e
Andrew Drummond <addrummond61@gmail.com>
2019-02-19 14:02:47 -0500

    completed erc20 tests

--- test/ERC20.js
+++ test/ERC20.js
@@ -18,30 +18,22 @@ contract("ERC20", accounts => {
 		erc20 = await ERC20.deployed("SlotToken", "SLT");
 	}
 
-	// creates a big number in wei, given a token input
-	// const tokensInWei = tokens => {
-	// 	// create a string representation of n
-	// 	const str = tokens + '0'.repeat(18);
-	// 	// return the big number representation
-	// 	return new BN(str, 10);
-	// }
-
 	describe("Initial state", () => {
 		beforeEach(deploy);
 
-		it("has a total supply", async () => {
+		it("totalSupply - starts with a total supply", async () => {
 			const totalSupply = await erc20.totalSupply.call();
 		 	assert.ok(totalSupply);
 		});
 
-		it("has a name and symbol", async () => {
+		it("name, symbol - starts with a name and symbol", async () => {
 			const name = await erc20.name.call();
 			const symbol = await erc20.symbol.call();
-			assert.equal(name, "SlotToken", "Name was not found");
-			assert.equal(symbol, "SLT", "Symbol was not found");
+			assert.equal(name, "SlotToken");
+			assert.equal(symbol, "SLT");
 		});
 
-		it("has only a small balance for deployer", async () => {
+		it("initialBalance - starts with a small balance for deployer", async () => {
 			const balanceInWei = await erc20.balanceOf.call(owner);
 			const balance = await web3.utils.fromWei(balanceInWei.toString(), "ether");
 			assert.equal(balance, 100);
@@ -51,30 +43,33 @@ contract("ERC20", accounts => {
 	describe("Contract functions", () => {
 		beforeEach(deploy);
 
-		it("has a function transfer, allowing a user to transfer tokens", async () => {
+		it("transfer - user can transfer tokens", async () => {
 			// grab the tokens in wei first
 			const tokensInWei = await web3.utils.toWei('10');
 			// owner will transfer 10 tokens to user1
 			await erc20.transfer(user1, tokensInWei, { from: owner });
+			// grab the balance of user1 in wei
 			const balanceInWei = await erc20.balanceOf.call(user1);
-			const balanceUser1 = await web3.utils.fromWei(balanceInWei.toString(), "ether");
-			assert.equal(balanceUser1, 10);
+			// convert the balance of user1 into eth
+			const balanceOfUser1 = await web3.utils.fromWei(balanceInWei.toString(), "ether");
+			// the balance should be 10 tokens
+			assert.equal(balanceOfUser1, 10);
 		});
 
-		it("has a function transfer, a user cannot transfer tokens they don't have", async () => {
+		it("transfer - a user cannot transfer tokens they don't have", async () => {
 			try {
 				// grab the tokens in wei first
 				const tokensInWei = await web3.utils.toWei('10');
 				// owner will transfer 10 tokens to user1
 				await erc20.transfer(user1, tokensInWei, { from: user2 });
 				// the test was supposed to throw an error
-				assert.fail("The test was supposed to fail!")
+				assert.fail("The test was supposed to throw an error!")
 			} catch (err) {
 				// if an error was thrown then we good
 			}
 		});
 
-		it("has a function approve, a user can set an allowance for another address", async () => {
+		it("transferFrom - a user can set an allowance for another address", async () => {
 			// grab the tokens in wei first
 			const tokensInWei = await web3.utils.toWei('10');
 			// owner will approve user1 to spend 10 tokens on their behalf
@@ -87,7 +82,74 @@ contract("ERC20", accounts => {
 			const balanceOfUser2 = await web3.utils.fromWei(balanceInWei);
 			// does it equal 10?
 			assert.equal(balanceOfUser2, 10);
-		})
+		});
+
+		it("transferFrom - an unapproved user cannot transfer another's tokens", async () => {
+			try {
+				// grab the tokens in wei first
+				const tokensInWei = await web3.utils.toWei('10');
+				// user1 can transfer 10 tokens to user2 on behalf of owner
+				await erc20.transferFrom(owner, user2, tokensInWei, { from: user1 });
+				// if we get here then something is wrong
+				assert.fail("The test was supposed to throw an error!")
+			} catch (err) {
+				// do nothing, the test was supposed to throw an error
+			}
+		});
+
+		it("burn - a user can destroy their tokens", async () => {
+			// grab the tokens in wei first
+			const tokensInWei = await web3.utils.toWei('10');
+			// grab the totalSupply
+			const totalSupply = await erc20.totalSupply.call();
+			// burn the tokens, from the owner
+			await erc20.burn(tokensInWei, { from: owner });
+			// grab the new totalSupply
+			const newTotalSupply = await erc20.totalSupply.call();
+			// did the total supply go down
+			assert.equal(totalSupply - tokensInWei, newTotalSupply);
+		});
+
+		it("burn - a user cannot destroy tokens if they don't have any", async () => {
+			try {
+				// grab the tokens in wei first
+				const tokensInWei = await web3.utils.toWei('10');
+				// burn the tokens, from the owner
+				await erc20.burn(tokensInWei, { from: user1 });
+				// if we get here then something is wrong
+				assert.fail("The test was supposed to throw an error!");
+			} catch (err) {
+				// do nothing, it's supposed to throw an error
+			}
+		});
+
+		it("burnFrom - a user can be approved to burn another address' tokens", async () => {
+			// grab the tokens in wei first
+			const tokensInWei = await web3.utils.toWei('10');
+			// grab the totalSupply
+			const totalSupply = await erc20.totalSupply.call();
+			// owner will approve user1 to spend 10 tokens on their behalf
+			await erc20.approve(user1, tokensInWei, { from: owner });
+			// user1 can transfer 10 tokens to user2 on behalf of owner
+			await erc20.burnFrom(owner, tokensInWei, { from: user1 });
+			// grab the new totalSupply
+			const newTotalSupply = await erc20.totalSupply.call();
+			// did the total supply go down
+			assert.equal(totalSupply - tokensInWei, newTotalSupply);
+		});
+
+		it("burnFrom - an unapproved user cannot destroy another's tokens", async () => {
+			try {
+				// grab the tokens in wei first
+				const tokensInWei = await web3.utils.toWei('10');
+				// burn the tokens, from the owner
+				await erc20.burnFrom(owner, tokensInWei, { from: user1 });
+				// if we get here then something is wrong
+				assert.fail("The test was supposed to throw an error!");
+			} catch (err) {
+				// do nothing, it's supposed to throw an error
+			}
+		});
 	})
 
 })
